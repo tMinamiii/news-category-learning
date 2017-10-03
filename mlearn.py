ad44deee3ced0777d06d93d1f208620bb88be972
naronA <otherthan.new@gmail.com>
2017-10-03 23:31:30 +0900

    辞書作成を日々行い易くするため、処理を分割する

--- mlearn.py
+++ mlearn.py
@@ -12,84 +12,73 @@ class TokenDictionary:
         self.token_dic = token_dic
         self.seq_no_uid = len(token_dic)
 
-    def convert2uid(self, manuscript_tokens: list):
-        manuscript_token_uid_list = []
-        for tok in manuscript_tokens:
-            self.update(tok)
-            manuscript_token_uid_list.append(self.token_dic[tok])
-        return manuscript_token_uid_list
-
     def update(self, manuscript_tokens: list):
         for tok in manuscript_tokens:
             if not tok in self.token_dic:
                 self.token_dic[tok] = self.seq_no_uid
                 self.seq_no_uid += 1
 
-
-class LearningData:
-    def __init__(self):
-        self.td = TokenDictionary()
-
-    def make(self, csv_list: list) -> list:
-        all_news = self.read_csv_data(csv_list)
-        learning_data = []
-        for news in all_news:
-            category = news[0]
-            vec_list = self.token_uid_list_2_vec_list(
-                news[1], self.td.seq_no_uid)
-            tf_vec = self.calc_norm_tf_vector(vec_list)
-            learning_data.append((category, tf_vec))
-        return learning_data
-
-    def dump_token_dic(self, filepath: str) -> None:
+    def dump(self, filepath: str) -> None:
         with open(filepath, mode='wb') as f:
-            pickle.dump(self.td.token_dic, f)
+            pickle.dump(self.token_dic, f)
 
-    def load_token_dic(self, filepath: str) -> None:
+    def load(self, filepath: str) -> None:
         with open(filepath, mode='rb') as f:
             loaded_dic = pickle.load(f)
-            self.td = TokenDictionary(loaded_dic)
+            self.token_dic = loaded_dic
 
-    def read_csv_data(self, csv_list: list, wc_lower: int=200) -> list:
+    def uid(self, token: str) -> int:
+        return self.token_dic[token]
+
+    def initialize(self, csv_list: list) -> list:
         '''
          ニュースのCSVを読み込んで、学習用データに変換する。
         ニュース原稿はTokenizeされ、更にuidのリスト(単なる数字のリスト)に変換される。
         ベクトルの最大次元数は、全データを読み込まないとわからないので、ここではまだTFベクトルに変換しない。
         '''
-        all_news = []
         for csv_path in csv_list:
             with open(csv_path, 'r') as f:
                 reader = csv.reader(f)
                 for row in reader:
-                    word_count = int(row[2])
-                    # 原稿の文字数が200文字以下なら飛ばす
-                    if word_count < wc_lower:
-                        continue
-                    category = row[0]
                     manuscript = row[3]
-                    token_list = self.tokenize(manuscript)
-                    self.td.update(token_list)
-                    token_uid_list = self.td.convert2uid(token_list)
-                    all_news.append((category, token_uid_list))
-        return all_news
+                    token_list = tokenize(manuscript)
+                    self.update(token_list)
 
-    def tokenize(self, manuscript: str) -> list:
-        token_list = []
-        tokenizer = Tokenizer()
-        tokens = tokenizer.tokenize(manuscript)
-        for tok in tokens:
-            ps = tok.part_of_speech.split(',')[0]
-            if not ps in ['名詞', '動詞', '形容詞']:
-                continue
-            # 原形があれば原形をリストに入れる
-            w = tok.base_form
-            if w == '*' or w == '':
-                # 原形がなければ表層系(原稿の単語そのまま)をリストに入れる
-                w = tok.surface
-            if w == '' or w == '\n':
-                continue
-            token_list.append(w)
-        return token_list
+
+class LearningData:
+    def __init__(self, token_dic: TokenDictionary):
+        self.token_dic = token_dic
+
+    def make(self, csv_list: list, wc_lower: int=200) -> list:
+        learning_data = []
+        for csv_path in csv_list:
+            data = self.read_csv(csv_path, wc_lower)
+            learning_data.extend(data)
+        return learning_data
+
+    def read_csv(self, csv_path: str, wc_lower: int=200) -> list:
+        learning_data = []
+        with open(csv_path, 'r') as f:
+            reader = csv.reader(f)
+            for line in reader:
+                wc = int(line[2])
+                if wc < wc_lower:
+                    continue
+                category = line[0]
+                manuscript = line[3]
+                token_list = tokenize(manuscript)
+                uid_list = self.token_2_uid(token_list)
+                vec_list = self.token_uid_list_2_vec_list(
+                    uid_list, self.token_dic.seq_no_uid)
+                tf_vec = self.calc_norm_tf_vector(vec_list)
+                learning_data.append((category, tf_vec))
+        return learning_data
+
+    def token_2_uid(self, manuscript_tokens: list)-> list:
+        manuscript_token_uid_list = []
+        for tok in manuscript_tokens:
+            manuscript_token_uid_list.append(self.token_dic.uid(tok))
+        return manuscript_token_uid_list
 
     def token_uid_list_2_vec_list(self, token_uid_list: list, max_dim: int) -> list:
         '''
@@ -111,7 +100,7 @@ class LearningData:
         平準化することで、長文にある1単語より、短文の1単語のほうが特徴に重みが付く。
         ※ただし、平準化は実験的な手法なので、もっといい方法があれば変更する
         '''
-        #total = np.zeros(self.td.seq_no_uid)
+        # total = np.zeros(self.td.seq_no_uid)
         total = None
         for vec in manuscript_vecs:
             if total is None:
@@ -120,3 +109,22 @@ class LearningData:
                 total += vec
         total /= len(manuscript_vecs)
         return total
+
+
+def tokenize(manuscript: str) -> list:
+    token_list = []
+    tokenizer = Tokenizer()
+    tokens = tokenizer.tokenize(manuscript)
+    for tok in tokens:
+        ps = tok.part_of_speech.split(',')[0]
+        if not ps in ['名詞', '動詞', '形容詞']:
+            continue
+        # 原形があれば原形をリストに入れる
+        w = tok.base_form
+        if w == '*' or w == '':
+            # 原形がなければ表層系(原稿の単語そのまま)をリストに入れる
+            w = tok.surface
+        if w == '' or w == '\n':
+            continue
+        token_list.append(w)
+    return token_list
