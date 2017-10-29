a1f4681e7debbce0456b1ff2bf65048d4f9becd7
naronA <otherthan.new@gmail.com>
2017-10-29 23:23:00 +0900

    定数をの日付と次元数を調整する

--- constant_values.py
+++ constant_values.py
@@ -1,6 +1,6 @@
 NUM_UNITS = 4000
-DATA_FILE = 'ldata/2017-10-28.td'
-PREPROCESSED_FILE = 'ldata/2017-10-28.prep'
+DATA_FILE = 'ldata/2017-10-29.td'
+PREPROCESSED_FILE = 'ldata/2017-10-29.prep'
 LOG_FILE = '/tmp/yn_categories_logs'
 BATCH_SIZE = 100
 TOTAL_STEP = 1000000
@@ -11,9 +11,9 @@ MINIMUM_TOKEN_LENGTH = 150
 KEEP_PROB = 0.5
 # smaller equal than BATCH_SIZE
 # 最大にしたい場合はNoneを設定する
-PCA_DIMENSION = 1500
+PCA_DIMENSION = 1000
 
 CATEGORIES = ['IT総合', '映画', '経済総合', '野球',
               '社会', 'ライフ総合', 'エンタメ総合', 'サッカー', 'スポーツ総合']
 # CATEGORIES = ['IT総合', '映画']
-PCA_BATCH_DATA_LENGTH = 1500
+PCA_BATCH_DATA_LENGTH = 1000
