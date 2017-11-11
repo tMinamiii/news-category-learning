46ba43c610be315d1d2a448f9424148a1251959a
naronA <otherthan.new@gmail.com>
2017-11-11 16:33:37 +0900

    メソッドを変数に定義しておいても、あまり効果がなかったので元に戻す

--- vectorize/vectorizer.py
+++ vectorize/vectorizer.py
@@ -98,43 +98,33 @@ class PCATfidfVectorizer:
         '''
         news_len = len(tokenized_news)
         random.shuffle(tokenized_news)
-        nparray = np.array
-        tfidf = self.tfidf
-        partial_fit = self.ipca.partial_fit
         for i in range(0, news_len, batch_size):
             chunks = tokenized_news[i:i + batch_size]
-            mat = nparray([tfidf(c[1]) for c in chunks])
-            partial_fit(mat)
+            mat = np.__version__array([self.tfidf(c[1]) for c in chunks])
+            self.ipca.partial_fit(mat)
 
     def vectorize(self, tokenized_news: list) -> np.array:
         '''
          ニュース原稿のTF-IDFベクトルを求めたのち主成分分析で次元削減する
         '''
         data = []
-        data_append = data.append
-        category_dic = self.prep.category_dic
-        transform = self.ipca.transform
-        tfidf = self.tfidf
         for category, counter in tokenized_news:
-            category_vec = category_dic[category]
-            tfidf = tfidf(counter).reshape(1, -1)
+            category_vec = self.prep.category_dic[category]
+            vec = self.tfidf(counter).reshape(1, -1)
             # transformの結果は2重リストになっているので、最初の要素を取り出す
-            dimred_tfidf = transform(tfidf)[0]
-            data_append((category_vec, dimred_tfidf))
+            dimred_tfidf = self.ipca.transform(vec)[0]
+            data.append((category_vec, dimred_tfidf))
         return np.array(data)
 
     def tfidf(self, token_counter: Counter) -> np.array:
         '''
         TF-IDFベクトルを求める。
         '''
-        prep = self.prep
         tf_vec = np.zeros(self.max_dim)
         total_tokens = sum(token_counter.values())
-        token_to_id = prep.token_to_id
-        idf = prep.idf
         for token, count in token_counter.items():
-            uid = token_to_id[token]
-            tf_vec[uid] = count / total_tokens * idf[token]
+            uid = self.prep.token_to_id[token]
+            tf_vec[uid] = count / total_tokens * self.prep.idf[token]
 
         return tf_vec
 
